f1274c16c868d02b09218a9742af673736862786
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-06-22 19:24:03 +0530

    final-touch

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
-import '../scss/styles.scss';
+import "../scss/styles.scss";
 
-import leaderBoard from './module/API.js';
-import postData from './module/postData.js';
-import getData from './module/getData.js';
+import leaderBoard from "./module/API.js";
+import postData from "./module/postData.js";
+import getData from "./module/getData.js";
 
-const form = document.querySelector('.form');
-const list = document.getElementById('container');
-const name = document.getElementById('name');
-const score = document.getElementById('score');
-const refreshBtn = document.getElementById('refresh_btn');
+const form = document.querySelector(".form");
+const list = document.getElementById("container");
+const name = document.getElementById("name");
+const score = document.getElementById("score");
+const refreshBtn = document.getElementById("refresh_btn");
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   const nameValue = name.value;
   const scoreValue = score.value;
   e.preventDefault();
@@ -20,16 +20,17 @@ form.addEventListener('submit', (e) => {
 
 const refreshScores = () => {
   getData().then((data) => {
-    list.innerHTML = '';
+    list.innerHTML = "";
     data.result.forEach((data) => {
-      const gameList = document.createElement('li');
+      const gameList = document.createElement("li");
+      gameList.classList.add("text-white-50", "bg-dark");
       gameList.innerHTML = `${data.user}: ${data.score}`;
       list.appendChild(gameList);
     });
   });
 };
 
-refreshBtn.addEventListener('click', refreshScores);
+refreshBtn.addEventListener("click", refreshScores);
 
 leaderBoard();
 
