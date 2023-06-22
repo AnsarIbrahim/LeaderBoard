0eea9a077386303f35dd58c7dac878d34048fcef
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-06-22 12:06:05 +0530

    update-async-method

--- src/js/module/API.js
+++ src/js/module/API.js
@@ -1,15 +1,24 @@
 const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
 
-const leaderBoard = () => {
-  fetch(baseURL, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-    body: JSON.stringify({ name: 'basketball' }),
-  })
-    .then((response) => response.json())
-    .then((json) => localStorage.setItem('gameId', JSON.stringify(json.result.split(' ')[3])))
-    .catch((error) => new Error('Error:', error));
+const leaderBoard = async () => {
+  try {
+    const response = await fetch(baseURL, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ name: 'basketball' }),
+    });
+    if (!response.ok) {
+      throw new Error('Error:', response.status);
+    }
+
+    const json = await response.json();
+    const gameId = json.result.split(' ')[3];
+    localStorage.setItem('gameId', JSON.stringify(gameId));
+  } catch (error) {
+    throw new Error(`An error occurred: ${error}`);
+  }
 };
+
 export default leaderBoard;
